1f68da391e333f117d94b7fb44940edff02bc229
etherealSunshine <aqarian77@gmail.com>
2024-06-21 17:54:11 +0530

    testing subtitles

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -9,7 +9,7 @@ export default function Messages() {
         if (msg.type === "user_message" || msg.type === "assistant_message") {
           return (
             <div key={msg.type + index} className="text-gray-700">
-              <div>{msg.message.role}</div>
+              <div>user: {msg.message.role}</div>
               <div>{msg.message.content}</div>
             </div>
           );
