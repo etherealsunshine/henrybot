9eaab27775344f96b1795ab97f33ff6cbe09f185
etherealSunshine <aqarian77@gmail.com>
2024-06-21 17:22:25 +0530

    added css and styling

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,33 +1,43 @@
 "use client";
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
+
 export default function Controls() {
   const { connect, disconnect, readyState } = useVoice();
 
   if (readyState === VoiceReadyState.OPEN) {
     return (
-      <button
-        onClick={() => {
-          disconnect();
-        }}
-      >
-        End Session
-      </button>
+      <div className="flex justify-center items-center min-h-screen bg-white">
+        <button
+          className="px-4 py-2 border rounded-lg text-black font-semibold"
+          onClick={() => {
+            disconnect();
+          }}
+        >
+          End Session
+        </button>
+      </div>
     );
   }
 
   return (
-    <button
-      onClick={() => {
-        connect()
-          .then(() => {
-            /* handle success */
-          })
-          .catch(() => {
-            /* handle error */
-          });
-      }}
-    >
-      Start Session
-    </button>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+      <h1 className="text-3xl mb-4 text-black">Talk to an Oxford student</h1>
+      
+      <p className="text-black mb-6">Press the Start Session button to talk, please keep in mind to wait a second or two after your response, to account for inference.</p>
+      <button
+        className="px-4 py-2 border rounded-lg text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        onClick={() => {
+          connect()
+           .then(() => {
+              /* handle success */
+            })
+           .catch(() => {
+              /* handle error */
+            });
+        }}
+      >
+        Start Session
+      </button>
+    </div>
   );
 }
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,15 +1,14 @@
 "use client";
 import { useVoice } from "@humeai/voice-react";
-
 export default function Messages() {
   const { messages } = useVoice();
 
   return (
     <div>
-      {messages.map((msg, index) => {
+      {messages.map((msg:any, index:any) => {
         if (msg.type === "user_message" || msg.type === "assistant_message") {
           return (
-            <div key={msg.type + index}>
+            <div key={msg.type + index} className="text-gray-700">
               <div>{msg.message.role}</div>
               <div>{msg.message.content}</div>
             </div>
